d42945fd06d70ee8868fa9d92b7db5c150d5ffa2
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-06-10 23:06:19 -0300

    criar repository para série

---
+++ src/main/java/com/example/screenmatch/repository/SerieRepository.java
@@ -0,0 +1,6 @@
+package com.example.screenmatch.repository;
+
+import com.example.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> { }
