e57d9924e502f1262f32d693abfcff60f6954233
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-06-10 23:06:05 -0300

    mapear a entidade Serie

--- src/main/java/com/example/screenmatch/model/Serie.java
+++ src/main/java/com/example/screenmatch/model/Serie.java
@@ -1,18 +1,41 @@
 package com.example.screenmatch.model;
 
-import com.example.screenmatch.service.ConsultaChatGPT;
 import com.example.screenmatch.service.ConsultaLibreTranslate;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
   private String titulo;
+
   private Integer totalTemporadas;
+
   private Double avaliacao;
+
+  @Enumerated(EnumType.STRING)
   private Categoria genero;
+
   private String atores;
+
   private String poster;
+
   private String sinopse;
+
+  @Transient
+  private List<Episodio> episodios = new ArrayList<>();
+
+  public Serie() {
+  }
 
   public Serie(DadosSerie dadosSerie) {
     this.titulo = dadosSerie.titulo();
@@ -21,7 +44,16 @@ public class Serie {
     this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
     this.atores = dadosSerie.atores();
     this.poster = dadosSerie.poster();
-    this.sinopse = ConsultaLibreTranslate.obterTraducao(dadosSerie.sinopse()).trim();
+//    this.sinopse = ConsultaLibreTranslate.obterTraducao(dadosSerie.sinopse()).trim();
+    this.sinopse = dadosSerie.sinopse();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public String getTitulo() {
@@ -78,6 +110,14 @@ public class Serie {
 
   public void setSinopse(String sinopse) {
     this.sinopse = sinopse;
+  }
+
+  public List<Episodio> getEpisodios() {
+    return episodios;
+  }
+
+  public void setEpisodios(List<Episodio> episodios) {
+    this.episodios = episodios;
   }
 
   @Override
