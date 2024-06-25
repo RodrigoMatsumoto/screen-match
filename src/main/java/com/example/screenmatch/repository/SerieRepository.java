63be7e06767786b296071fcb41f18fce643b353a
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-06-25 20:22:28 -0300

    buscar episódios por trecho do título

--- src/main/java/com/example/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/example/screenmatch/repository/SerieRepository.java
@@ -3,4 +3,9 @@ package com.example.screenmatch.repository;
 import com.example.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SerieRepository extends JpaRepository<Serie, Long> { }
+import java.util.Optional;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+  Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+}
