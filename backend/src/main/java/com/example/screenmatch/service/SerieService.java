c7642932e610aab668e01675f8bc9f633931c33b
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-06-29 01:03:51 -0300

    criar service para serie

---
+++ backend/src/main/java/com/example/screenmatch/service/SerieService.java
@@ -0,0 +1,40 @@
+package com.example.screenmatch.service;
+
+import com.example.screenmatch.dto.SerieDTO;
+import com.example.screenmatch.model.Serie;
+import com.example.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+  @Autowired
+  private SerieRepository serieRepository;
+
+  public List<SerieDTO> obterTodasAsSeries() {
+    return converterDados(serieRepository.findAll());
+  }
+
+  public List<SerieDTO> obterTop5Series() {
+    return converterDados(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+  }
+
+  private List<SerieDTO> converterDados(List<Serie> series) {
+    return series.stream()
+        .map(serie -> new SerieDTO(
+             serie.getId(),
+             serie.getTitulo(),
+             serie.getTotalTemporadas(),
+             serie.getAvaliacao(),
+             serie.getGenero(),
+             serie.getAtores(),
+             serie.getPoster(),
+             serie.getSinopse()
+            )
+        ).collect(Collectors.toList());
+  }
+}
