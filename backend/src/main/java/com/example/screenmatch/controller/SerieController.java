4c53b153946821ddce890d9c99eb46558974ee4b
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-06-29 01:04:39 -0300

    refatorar controller de serie

--- backend/src/main/java/com/example/screenmatch/controller/SerieController.java
+++ backend/src/main/java/com/example/screenmatch/controller/SerieController.java
@@ -1,34 +1,28 @@
 package com.example.screenmatch.controller;
 
 import com.example.screenmatch.dto.SerieDTO;
-import com.example.screenmatch.repository.SerieRepository;
+import com.example.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
   @Autowired
-  private SerieRepository serieRepository;
+  private SerieService serieService;
 
-  @GetMapping("/series")
+  @GetMapping
   public List<SerieDTO> obterSeries() {
-    return serieRepository.findAll()
-        .stream()
-        .map(serie -> new SerieDTO(
-             serie.getId(),
-             serie.getTitulo(),
-             serie.getTotalTemporadas(),
-             serie.getAvaliacao(),
-             serie.getGenero(),
-             serie.getAtores(),
-             serie.getPoster(),
-             serie.getSinopse()
-            )
-        ).collect(Collectors.toList());
+    return serieService.obterTodasAsSeries();
+  }
+
+  @GetMapping("/top5")
+  public List<SerieDTO> obterTop5Series() {
+    return serieService.obterTop5Series();
   }
 }
